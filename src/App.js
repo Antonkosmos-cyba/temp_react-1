c35777ccd5c084e03d79906c2e46c91d36453660
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-09-01 14:38:11 +0300

    route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Main from "./pages/main/Main";
+import Roknrol from "./pages/roknrol/Roknrol";
+
 function App() {
   return (
     <>
-      <h1>nastya</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Main />} />
+            <Route path="/roknrol" element={<Roknrol />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
