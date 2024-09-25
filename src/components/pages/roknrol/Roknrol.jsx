924a2524499f25157a3d63a9e2d9084c5ce4b20d
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-09-25 15:31:13 +0300

    import muz

--- src/components/pages/roknrol/Roknrol.jsx
+++ src/components/pages/roknrol/Roknrol.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-
-import "./roknrol";
 import "./roknrol.css";
+import "./player/player.css";
+import Player from "./player/player";
+// import Player2 from "./player/player_2";
 
 function Roknrol() {
   return (
     <>
-      <div className="anta"></div>
+      <Player />
+      {/* <Player2 /> */}
     </>
   );
 }
