b9348b3eaa2846e1fb96029d2a84bef61a249525
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-10-13 13:48:01 +0300

    font

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
 import "./main.css";
+import "../../app.css";
 
 function Main() {
   return (
     <>
-      <div className="nasta"> Anastasija</div>
+      <div className="nasta">
+        <div>
+          <p className="topic">
+            Anastasija Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ex facilis dolor expedita praesentium libero odit nostrum laboriosam
+            voluptate, illum aliquam quis quos nulla ut neque dignissimos
+            reiciendis incidunt eveniet? Voluptatibus rerum vel quis voluptates
+            velit eaque unde aliquam ducimus eius incidunt, id sit ipsam est,
+            animi in excepturi sequi quasi sapiente autem laboriosam corporis
+            cupiditate modi placeat! Ratione minus, laboriosam animi rerum
+            officia rem eaque eius velit aperiam nesciunt at corporis error
+            cupiditate voluptas quos. Quas animi dolores tempore, possimus
+            consequuntur autem eius iusto dolor, architecto illo corrupti
+            commodi, officiis harum. Perferendis nam assumenda quam, at quia
+            voluptatem odit possimus.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
