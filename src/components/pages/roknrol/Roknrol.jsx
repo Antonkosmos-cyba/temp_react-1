691ed3ee4ea41a8522fa8037e23bccad7983fc3d
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-10-17 11:29:37 +0300

    player css

--- src/components/pages/roknrol/Roknrol.jsx
+++ src/components/pages/roknrol/Roknrol.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./roknrol.css";
-import "./player/player.css";
+import "../player.css";
 import Player from "./player/player";
 import Player2 from "./player/player_2";
 
