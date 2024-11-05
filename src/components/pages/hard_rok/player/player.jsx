d56c2d7f91c42f31ac80bf3407af3296bcdf4342
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-11-05 16:09:03 +0300

    audio Stand

--- src/components/pages/hard_rok/player/player.jsx
+++ src/components/pages/hard_rok/player/player.jsx
@@ -1,3 +1,10 @@
+// import React,{ useState } from "react";
+// import ReactAudioPlayer from "react-audio-player";
+
+import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
+
+import { Button } from "primereact/button";
+
 import wsogg from "../ws.ogg";
 import wswebp from "../ws.webp";
 
@@ -10,7 +17,19 @@ export default function Player() {
         <p className="subTitle">White Snake</p>
       </div>
       <div>
-        <audio className="antona" src={wsogg} controls></audio>
+        {/* <ReactAudioPlayer src={wsogg} controls /> */}
+        {/* <audio className="antona" src={wsogg} controls></audio> */}
+        <audio id="audioPlayer" src={wsogg}></audio>
+
+        <Button icon="pi pi-check" rounded text raised aria-label="Filter" />
+        <Button
+          icon="pi pi-times"
+          rounded
+          text
+          raised
+          severity="danger"
+          aria-label="Cancel"
+        />
       </div>
     </div>
   );
